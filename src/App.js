be142f7e641102acb10530f9097ea4b3c4c8588f
Cezaniltom <cezaniltom@outlook.com>
2024-05-28 08:32:24 -0300

    Adicionando components Button and Input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 
-function App() {
+import Input from './components/Input'
+import Button from './components/Button'
+
+import { Container, Content } from './styles';
+
+const App = () => {
   return (
-    <div className="App">
-      Olá
-    </div>
+    <Container>
+      <Content>
+        <Input />
+      </Content>
+    </Container>
   );
 }
 
