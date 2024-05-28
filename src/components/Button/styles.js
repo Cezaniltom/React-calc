b8a0fa84aa324932fca4d9a9182854801f006ab1
Cezaniltom <cezaniltom@outlook.com>
2024-05-28 15:45:06 -0300

    Adicionando números e operações lógicas

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,1 +1,12 @@
 import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: #00AAAA;
+    cursor: pointer;
+    color: #DDDDDD;
+    font-size: 24px;
+    font-wight: 700;
+    flex: 1;
+`
